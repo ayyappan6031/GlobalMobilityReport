e56503655d6bb5bef7d302f7abc6367aa43dfe80
Ayyappan <ayyappan6031@gmail.com>
2021-05-26 00:10:41 +0530

    source download file added

---
+++ download_source.py
@@ -0,0 +1,12 @@
+from tqdm import *
+import requests
+url = "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv"
+name = 'Global_Mobility_Report.csv'
+with requests.get(url, stream=True) as r:
+    r.raise_for_status()
+    with open(name, 'wb') as f:
+        pbar = tqdm(total=int(r.headers['Content-Length']))
+        for chunk in r.iter_content(chunk_size=8192):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+                pbar.update(len(chunk))
